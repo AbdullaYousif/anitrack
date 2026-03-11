72fb1f7c25d1f31dfd5cd36b333a26071c2eee2e
AbdullaYousif <78888941+AbdullaYousif@users.noreply.github.com>
2026-03-10 21:04:52 -0400

    Improved watchlist status with completed, plan to watch, or currently watching, and added local caching with useRef

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-function AnimeCard({ anime, inWatchlist, onToggle }) {
+function AnimeCard({ anime, inWatchlist, onToggle, status, onChangeStatus }) {
   return (
     <div className="bg-gray-900 text-white flex flex-col rounded-xl overflow-hidden ">
       <img
@@ -15,6 +15,13 @@ function AnimeCard({ anime, inWatchlist, onToggle }) {
         <button onClick={onToggle} className={` mt-auto w-full py-1 rounded text-sm font-semibold ${inWatchlist ? "bg-gray-700 hover:bg-red-500" : "bg-sky-500 hover:bg-sky-400"}`}>
           {inWatchlist? "Remove from Watchlist" : "Add to Watchlist"}
         </button>
+        {inWatchlist && <select className="bg-gray-800 text-white text-sm rounded mt-2 w-full p-1 outline-none border border-gray-700" value={status} onChange={(e => onChangeStatus(e.target.value))} name="watchlistStatus" id="">
+            <option value="Plan to Watch">Plan to Watch</option>
+            <option value="Watching">Currently Watching</option>
+            <option value="Completed">Completed</option>
+          </select>
+        }
+          
       </div>
     </div>
   );
