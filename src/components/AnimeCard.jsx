a2a70de323cab002e5cf51d442d1233104925864
AbdullaYousif <78888941+AbdullaYousif@users.noreply.github.com>
2026-03-20 20:17:23 -0400

    added seasonal anime and button hover effects

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -12,10 +12,10 @@ function AnimeCard({ anime, inWatchlist, onToggle, status, onChangeStatus }) {
         <p className="text-lg font-semibold line-clamp-2">{anime.title}</p>
         <p className="text-base text-gray-500">{anime.score ?? "N/A"}</p>
 
-        <button onClick={onToggle} className={` mt-auto w-full py-1 rounded text-sm font-semibold ${inWatchlist ? "bg-gray-700 hover:bg-red-500" : "bg-sky-500 hover:bg-sky-400"}`}>
+        <button onClick={onToggle} className={` cursor-pointer mt-auto w-full py-1 rounded text-sm font-semibold ${inWatchlist ? "bg-gray-700 hover:bg-red-500" : "bg-sky-500 hover:bg-sky-400"}`}>
           {inWatchlist? "Remove from Watchlist" : "Add to Watchlist"}
         </button>
-        {inWatchlist && <select className="bg-gray-800 text-white text-sm rounded mt-2 w-full p-1 outline-none border border-gray-700" value={status} onChange={(e => onChangeStatus(e.target.value))} name="watchlistStatus" id="">
+        {inWatchlist && <select className=" cursor-pointer bg-gray-800 text-white text-sm rounded mt-2 w-full p-1 outline-none border border-gray-700" value={status} onChange={(e => onChangeStatus(e.target.value))} name="watchlistStatus" id="">
             <option value="Plan to Watch">Plan to Watch</option>
             <option value="Watching">Currently Watching</option>
             <option value="Completed">Completed</option>
